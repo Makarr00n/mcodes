239984ab3d4e808647567e94633c2c4be39841d7
Makarr00n <115668880+Makarr00n@users.noreply.github.com>
2023-10-30 10:19:16 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,9 @@
+const verplan=()=>{
+    document.getElementById("cartas").style.display="flex"
+}
+const vermas=()=>{
+    document.getElementById("cartas2").style.display="flex"
+}
+const tienda1=()=>{
+    document.getElementById("tienda1").style.display="flex"
+}
